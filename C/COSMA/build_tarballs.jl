3f1995ef35bd46d619e01d1a4cbdfbb48ca10d7d
Harmen Stoppels <harmenstoppels@gmail.com>
2020-05-16 17:52:18 +0200

    New Recipe: COSMA v2.2.0

---
+++ C/COSMA/build_tarballs.jl
@@ -0,0 +1,81 @@
+# Note that this script can accept some limited command-line arguments, run
+# `julia build_tarballs.jl --help` to see a usage message.
+using BinaryBuilder, Pkg
+
+name = "COSMA"
+version = v"2.2.0"
+
+# Collection of sources required to complete build
+sources = [
+    ArchiveSource("https://github.com/eth-cscs/COSMA/releases/download/v2.2.0/cosma.tar.gz", "1eb92a98110df595070a12193b9221eecf9d103ced8836c960f6c79a2bd553ca"),
+    DirectorySource("./bundled")
+]
+
+# Bash recipe for building across all platforms
+script = raw"""
+cd $WORKSPACE/srcdir
+for f in ${WORKSPACE}/srcdir/patches/*.patch; do
+    atomic_patch -p1 ${f}
+done
+cd cosma/src/cosma/
+ls
+vim blas.cpp 
+vim ../../CMakeLists.txt 
+vim blas.cpp 
+ldd $destir/include/openblas
+ls $destir/include/openblas
+ls $destir/include/openblas*
+ls $destir
+ls ${WORKSPACE}
+ls ${WORKSPACE}/destdir
+ls ${WORKSPACE}/destdir/include/openblas_config.h 
+cat ${WORKSPACE}/destdir/include/openblas_config.h 
+strings ${WORKSPACE}/destdir/lib/libopenblas64_.dylib | grep cgemm
+cat ${WORKSPACE}/destdir/include/openblas_config.h 
+cat ${WORKSPACE}/destdir/include/openblas_config.h 
+cat ${WORKSPACE}/destdir/include/cblas.h | grep cblas_dgemm
+cat ${WORKSPACE}/destdir/include/cblas.h | grep cblas_dgemm - n2
+cat ${WORKSPACE}/destdir/include/cblas.h | grep cblas_dgemm -n2
+cat ${WORKSPACE}/destdir/include/cblas.h | grep cblas_.gemm -n2
+vim blas.cpp 
+gs
+git status
+cd ..
+cd ..
+ls
+vim CMakeLists.txt 
+ls
+ls
+cd ../
+ls
+if [[ "$nbits" == "64" ]] && [[ "$target" != aarch64-* ]]; then     BLAS_LAPACK_LIB="$libdir/libopenblas64_.$dlext"; else     BLAS_LAPACK_LIB="$libdir/libopenblas.$dlext"; fi
+if [[ "$target" == x86_64-apple-darwin14 ]] || [[ "$target" == x86_64-unknown-freebsd11.1 ]]; then     OMP_LIB=`find $libdir -iname 'libgomp*'`;     OPENMP_CMAKE_FLAGS="-DOpenMP_CXX_FLAGS=-fopenmp=libgomp -DOpenMP_CXX_LIB_NAMES=gomp -DOpenMP_gomp_LIBRARY=$OMP_LIB";     OMP_HEADER=`find / -name omp.h 2>/dev/null  | head -n1`;     mkdir include;     cp $OMP_HEADER include/;     export CPATH=$PWD/include; else     OPENMP_CMAKE_FLAGS=; fi
+mkdir build
+cd build
+cmake ../cosma     -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TARGET_TOOLCHAIN}     -DCMAKE_BUILD_TYPE=Release     -DCMAKE_INSTALL_PREFIX=$prefix     -DCOSMA_WITH_TESTS=OFF     -DCOSMA_WITH_APPS=OFF     -DCOSMA_WITH_BENCHMARKS=OFF     -DCOSMA_WITH_INSTALL=ON     -DCOSMA_WITH_PROFILING=OFF     -DBUILD_SHARED_LIBS=ON     -DCOSMA_BLAS=OPENBLAS     -DCOSMA_SCALAPACK=OFF     -DMPI_CXX_COMPILER=$bindir/mpicxx     -DMPI_C_COMPILER=$bindir/mpicc     -DOPENBLAS_LIBRARIES=$BLAS_LAPACK_LIB     -DOPENBLAS_INCLUDE_DIR=$libdir/../include     $OPENMP_CMAKE_FLAGS
+make -j16
+make install
+"""
+
+# These are the platforms we will build for by default, unless further
+# platforms are passed in on the command line
+platforms = [
+    MacOS(:x86_64)
+]
+
+
+# The products that we will ensure are always built
+products = [
+    LibraryProduct("libgrid2grid", :grid2grid),
+    LibraryProduct("libcosma", :cosma)
+]
+
+# Dependencies that must be installed before this package can be built
+dependencies = [
+    Dependency(PackageSpec(name="OpenBLAS_jll", uuid="4536629a-c528-5b80-bd46-f80d51c5b363"))
+    Dependency(PackageSpec(name="MPICH_jll", uuid="7cb0a576-ebde-5e09-9194-50597f1243b4"))
+    Dependency(PackageSpec(name="CompilerSupportLibraries_jll", uuid="e66e0078-7015-5450-92f7-15fbd957f2ae"))
+]
+
+# Build the tarballs, and possibly a `build.jl` as well.
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies)
